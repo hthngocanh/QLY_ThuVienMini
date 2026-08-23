d72e843fa139bebb72eda66aef903db4a75f94b2
Hoàng Thị Ngọc Ánh <ngoccanhh2206@gmail.com>
2026-08-23 23:20:19 +0700

    phieumuon

--- database/tables/borrow_slips.sql
+++ database/tables/borrow_slips.sql
@@ -1,15 +1,25 @@
 CREATE TABLE borrow_slips (
     ID_PhieuMuon INT IDENTITY(1,1) PRIMARY KEY,
+
     ID_NguoiDung INT NOT NULL,
     ID_BanSao INT NOT NULL,
+
     NgayMuon DATE NOT NULL,
     NgayTra DATE NULL,
+
     TrangThai NVARCHAR(20) NOT NULL
         CHECK (TrangThai IN (
             N'Chờ duyệt',
             N'Đang mượn',
             N'Quá hạn',
             N'Đã trả'
-        ))
+        )),
+
+    FOREIGN KEY (ID_NguoiDung)
+        REFERENCES nguoi_dung(id),
+
+    FOREIGN KEY (ID_BanSao)
+        REFERENCES book_copies(id)
 );
+
 GO
