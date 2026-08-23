22198016823bbc11e2607900ca49dd7c5ee24903
linhpham0706 <phamkhanhlinh07062006@gmail.com>
2026-08-23 20:41:29 +0700

    feat: add dữ liệu mẫu danh muc sácha

---
+++ database/seed/ategories_seed.sql
@@ -0,0 +1,22 @@
+INSERT INTO Categories
+    (ten_danh_muc, mo_ta, trang_thai)
+VALUES
+(N'Công nghệ thông tin',
+ N'Sách về máy tính và lập trình',
+ N'Hoạt động'),
+
+(N'Văn học',
+ N'Sách văn học',
+ N'Hoạt động'),
+
+(N'Ngoại ngữ',
+ N'Sách học ngoại ngữ',
+ N'Hoạt động'),
+
+(N'Kinh tế',
+ N'Sách về kinh tế',
+ N'Hoạt động'),
+
+(N'Khoa học',
+ N'Sách về khoa học',
+ N'Hoạt động');
