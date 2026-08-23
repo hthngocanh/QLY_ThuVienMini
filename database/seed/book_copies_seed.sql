8ac777adccd03cc14cf0589451e9d6650071f6a5
Tranng-nocry <trantrang682006@gmail.com>
2026-08-23 23:26:26 +0700

    feat: add book copies seed data

---
+++ database/seed/book_copies_seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO book_copies
+    (book_id, ma_ban_sao, vi_tri, trang_thai)
+VALUES
+    (1, 'BS001', N'Kệ A1', N'Có sẵn'),
+    (1, 'BS002', N'Kệ A1', N'Đang mượn'),
+    (2, 'BS003', N'Kệ B2', N'Có sẵn'),
+    (3, 'BS004', N'Kệ C1', N'Hỏng');
